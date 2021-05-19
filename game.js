d1c4f384f6d07561cc2c02178f21dc80b6d3cf8f
ishika <imusun24@gmail.com>
2021-05-20 03:31:46 +0530

    creating a new pattern

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-alert("working!");
+var gamePatter = [];
+
+//array of colors?
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomNumber = Math.random();
+  randomNumber = randomNumber * 4;
+  randomNumber = Math.floor(randomNumber);
+}
+
+//we chose random number from randomnumber and color from buttonColors
+var randomChosenColours=buttonColours[randomNumber];
+
+//for the game pattern,we push new chosen random color
+gamePattern.push(randomChosenColours);
