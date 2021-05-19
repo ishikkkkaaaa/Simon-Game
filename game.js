c21c172e420c544efec8e57e99a5651553d194fc
ishika <imusun24@gmail.com>
2021-05-20 04:02:38 +0530

    button pressed

--- game.js
+++ game.js
@@ -1,12 +1,23 @@
 var gamePatter = [];
-
+var userClickedPattern = [];
 //array of colors?
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+$(".btn").click(function () {
+  //store the id of the button that got clicked.
+  var userChosenColour = $(this).attr("id");
+  //adding chosen color to new clicked pattern
+  userClickedPattern.push(userChosenColour);
+});
+
 function nextSequence() {
-  var randomNumber = Math.random();
-  randomNumber = randomNumber * 4;
-  randomNumber = Math.floor(randomNumber);
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  //we chose random number from randomnumber and color from buttonColors
+  var randomChosenColour = buttonColours[randomNumber];
+
+  //for the game pattern,we push new chosen random color
+  gamePattern.push(randomChosenColour);
 
   //selecting the button w same ID using jQuery
   $("#" + randomChosenColour)
@@ -18,9 +29,3 @@ function nextSequence() {
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 }
-
-//we chose random number from randomnumber and color from buttonColors
-var randomChosenColours = buttonColours[randomNumber];
-
-//for the game pattern,we push new chosen random color
-gamePattern.push(randomChosenColours);
