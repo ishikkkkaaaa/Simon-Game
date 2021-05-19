9a5ab4e7dba562f0045a73b7c643ad758636f518
ishika <imusun24@gmail.com>
2021-05-20 03:47:14 +0530

    Sequence to the User with Animations and Sounds

--- game.js
+++ game.js
@@ -7,10 +7,20 @@ function nextSequence() {
   var randomNumber = Math.random();
   randomNumber = randomNumber * 4;
   randomNumber = Math.floor(randomNumber);
+
+  //selecting the button w same ID using jQuery
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  //adding audio
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
 
 //we chose random number from randomnumber and color from buttonColors
-var randomChosenColours=buttonColours[randomNumber];
+var randomChosenColours = buttonColours[randomNumber];
 
 //for the game pattern,we push new chosen random color
 gamePattern.push(randomChosenColours);
